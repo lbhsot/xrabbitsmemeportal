980a033a71ffe1b70d7ed857f58c4bd75b18cb0e
libinghan <libinghan@cobo.com>
2022-02-20 20:43:50 +0800

    init project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <ImageGenerator msg="Welcome to Your Vue.js + TypeScript App"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import ImageGenerator from '@/components/ImageGenerator.vue'; // @ is an alias to /src
+
+@Options({
+  components: {
+    ImageGenerator,
+  },
+})
+export default class HomeView extends Vue {}
+</script>
